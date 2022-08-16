8f350347355cd67ecef98a1ba63514eab1e60db6
NowrinIslamMim <nowrin02@gmail.com>
2022-08-16 21:15:20 +0600

    final

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,25 +7,39 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
+    if (isNaN(newWithdrawAmount)) {
+        alert('Input a valid taka amount');
+        return;
+    }
+
+    //step-7 clear the deposit field
+    withdrawField.value = '';
+
     //step-3 update the withdraw value
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    //step-4 add the sum
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     //step-5 get the balance field
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceString = balanceTotal.innerText;
     const previousBalance = parseFloat(previousBalanceString);
 
+    if (newWithdrawAmount > previousBalance) {
+        alert('Bapp er bank e eto taka naii');
+        return;
+    }
+
+    //step-4 add the sum
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     //step-6 update the current balance
     const currentBalance = previousBalance - newWithdrawAmount;
     balanceTotal.innerText = currentBalance;
 
-    //step-7 clear the deposit field
-    withdrawField.value = '';
+
 
 })
